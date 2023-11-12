13e134086c096d1fb30bb1c57c61b950540b6d3f
Vitalii Rubtsov <proektant.burenie@gmail.com>
2023-11-12 11:25:02 +0700

    add new directory "./frontend/src/components" with some new pages

---
+++ frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,5 @@
+const ErrorPage = () => (
+    <h1>Error 404. Page is not found</h1>
+);
+
+export default ErrorPage;
---
+++ frontend/src/components/PublicPage.jsx
@@ -0,0 +1,5 @@
+const PublicPage = () => (
+    <h1>Public page</h1>
+);
+
+export default PublicPage;

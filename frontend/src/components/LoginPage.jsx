04dc80a530f2f199f43669553a5fee4c280a17db
Vitalii Rubtsov <proektant.burenie@gmail.com>
2023-11-12 21:42:50 +0700

    update LoginPage.jsx in components

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,5 +1,38 @@
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Минимум 2 буквы')
+    .max(50, 'Максимум 50 букв')
+    .required('Обязательное поле'),
+  password: Yup.string()
+    .password('Неверный пароль')
+    .required('Обязательное поле'),
+});
+
 const LoginPage = () => (
-  <h1>LogIn</h1>
+  <Formik
+    initialValues={{ username: '', password: '' }}
+    validationSchema={SignupSchema}
+    onSubmit={(values) => {
+      console.log(values); // eslint-disable-line
+    }}
+  >
+    {({ errors, touched }) => (
+      <Form>
+        <Field name="username" />
+        {errors.username && touched.username ? (
+          <div>{errors.username}</div>
+        ) : null}
+        <Field name="password" />
+        {errors.password && touched.password ? (
+          <div>{errors.password}</div>
+        ) : null}
+        <button type="submit">Submit</button>
+      </Form>
+    )}
+  </Formik>
 );
 
 export default LoginPage;

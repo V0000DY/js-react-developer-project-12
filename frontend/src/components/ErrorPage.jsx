c6d5d373c0b5214d6b2dd783c4ca815b1246d612
Vitalii Rubtsov <proektant.burenie@gmail.com>
2023-11-14 21:16:12 +0700

    update ErrorPage

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -1,5 +1,11 @@
 const ErrorPage = () => (
-  <h1>Error 404. Page not found!</h1>
+  <div className="container-fluid">
+    <div className="row justify-content-center pt-5">
+      <div className="col-sm-4">
+        <h1>Error 404. Page not found!</h1>
+      </div>
+    </div>
+  </div>
 );
 
 export default ErrorPage;

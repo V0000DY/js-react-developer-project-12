a392a990443b26c3b16737d8b4eef29edb41a484
Vitalii Rubtsov <proektant.burenie@gmail.com>
2023-11-26 20:01:42 +0700

    update publicPage with svg-elements on buttons

--- frontend/src/components/PublicPage.jsx
+++ frontend/src/components/PublicPage.jsx
@@ -8,7 +8,6 @@ import {
   Col,
   Form,
   InputGroup,
-  Button,
 } from 'react-bootstrap';
 import TopLine from './NavBarComp.jsx';
 
@@ -61,8 +60,11 @@ const PublicPage = () => {
     return (
       <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
-          <p className="m-">
-            <b>{currentChannelName}</b>
+          <p className="m-0">
+            <b>
+              {'# '}
+              {currentChannelName}
+            </b>
           </p>
           <span className="text-muted">{`${messages.length} сообщений`}</span>
         </div>
@@ -85,10 +87,21 @@ const PublicPage = () => {
                 name="body"
                 className="border-0 p-0 ps-2"
               />
-              <Button type="submit" className="btn btn-group-vertical">
-                --&gt
+              <button type="submit" disabled className="btn btn-group-vertical border-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                  />
+                </svg>
                 <span className="visually-hidden">Отправить</span>
-              </Button>
+              </button>
             </InputGroup>
           </Form>
         </div>
@@ -104,7 +117,23 @@ const PublicPage = () => {
           <Col xs={4} md="2" className="border-end px-0 bg-light flex-column d-flex">
             <div className="d-flex mt-1 mb-2 ps-4 pe-2 p-4 justify-content-between">
               <b>Каналы</b>
-              <b>+</b>
+              <button type="button" className="p-0 text-primary btn btn-group-vertical">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                  />
+                  <path
+                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                  />
+                </svg>
+                <span className="visually-hidden">+</span>
+              </button>
             </div>
             {renderItems(content.channels)}
           </Col>

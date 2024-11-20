77aaf49313023e4c1a2ea7f6d8b86b1937b7f24c
V0000DY <proektant.burenie@gmail.com>
2024-11-20 12:22:02 +0700

    separated texts from ui

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -2,13 +2,12 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from './locales/index.js';
 
-const defaultLanguage = 'ru';
 const i18nInstance = i18n.createInstance();
 
-i18nInstance
+await i18nInstance
   .use(initReactI18next)
   .init({
-    lng: defaultLanguage,
+    lng: 'ru',
     resources,
   });
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './i18n.js';
+import { I18nextProvider } from 'react-i18next';
 import './index.css';
 
 import App from './App.jsx';
 import store from './services/index.jsx';
+import i18nInstance from './i18n.js';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <I18nextProvider i18n={i18nInstance}>
+      <App />
+    </I18nextProvider>
   </Provider>,
 );

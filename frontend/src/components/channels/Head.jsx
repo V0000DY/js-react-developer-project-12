b8c04b02deb51d76b8fecdf44ef5f5bc33ee8d86
V0000DY <proektant.burenie@gmail.com>
2024-12-16 12:14:24 +0700

    fix addChannel button

--- frontend/src/components/channels/Head.jsx
+++ frontend/src/components/channels/Head.jsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import plus from '../../assets/Plus.svg';
+import Plus from '../../assets/Plus';
 
 const ChannelsTabHead = ({ showModal }) => {
   const { t } = useTranslation();
@@ -9,7 +9,7 @@ const ChannelsTabHead = ({ showModal }) => {
     <div className="d-flex mt-1 mb-2 ps-4 pe-2 p-4 justify-content-between">
       <b>{t('channelsTab.channelsList.title')}</b>
       <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => showModal('adding')}>
-        <img src={plus} alt={t('channelsTab.channelsList.addButton')} />
+        <Plus />
         <span className="visually-hidden">{t('channelsTab.channelsList.addButton')}</span>
       </button>
     </div>
